b2e9362b40414bf90cc1c9262c0261436353e949
Cristhopher <Danielcristhopher@gmai.com>
2020-12-15 22:42:08 -0400

    Âproblem fixed

--- src/App.js
+++ src/App.js
@@ -15,12 +15,12 @@ function App() {
 
     {/*------These are the suggestions of frends--------*/}
     <Follow myName="Chris Daniel" myUserName="chr1s__" myImg={images[0].userImg}>
-            {images.map(el=> <UserToFollow image={el.userImg} name={el.user}  type="Suregerencias para ti"/>)}
+            {images.map((el,index)=> <UserToFollow key={index + '-' + el.user} image={el.userImg} name={el.user}  type="Suregerencias para ti"/>)}
     </Follow>
 
     {/*------Here are the historys--------*/}
     <AllHistorys>
-           {images.map(el=> <History imageProfile={el.userImg} userName={el.user}/>)}
+           {images.map((el,index)=> <History key={index + '-' + el.user} imageProfile={el.userImg} userName={el.user}/>)}
             <History imageProfile={images[1].userImg} userName="punteroww"/>
             <History imageProfile={images[1].userImg} userName="programer"/>
             <History imageProfile={images[1].userImg} userName="theemprendedor"/>
@@ -31,7 +31,7 @@ function App() {
     </AllHistorys>
 
     {/*------The posts--------*/}
-    {images.map(el=> <Post userImg={el.userImg} userName={el.user} postImg={el.url} caption="this is a example" likes="45.200" time="7 HORAS"account="chr1__" />)}
+    {images.map((el,index)=> <Post key={index + '-' + el.user} userImg={el.userImg} userName={el.user} postImg={el.url} caption="this is a example" likes="45.200" time="7 HORAS"account="chr1__" />)}
    </>
   );
 }
